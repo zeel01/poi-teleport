31aa88facc94d50af6ddfa9a9f5510ef75efd9ea
zeel <zeel02@gmail.com>
2020-09-21 13:54:31 -0400

    Basic functionality, loads the scene the note is associated with on right-click.

--- poi-teleport.js
+++ poi-teleport.js
@@ -1,3 +1,31 @@
 /** 
  * Point of Interest Teleporter
  */
+
+class PointOfInterestTeleporter {
+	static onReady() {
+		canvas.notes.placeables.forEach(n => this.checkNote(n));
+
+		console.log(game.i18n.localize("poitp.name"), "| Ready.");	
+	}
+	static checkNote(note) {
+		const scene = game.scenes.find(s => s.data.journal == note.entry.id);
+		if (scene) new PointOfInterestTeleporter(note, scene); 
+	}
+
+	constructor(note, scene) {
+		this.note = note;
+		this.scene = scene;
+
+		this.activateListeners();
+		
+	}
+	activateListeners() {
+		this.note.mouseInteractionManager.target.on("rightdown", this._contextMenu.bind(this));
+	}
+	_contextMenu() {
+		this.scene.activate();
+	}
+}
+
+Hooks.on("canvasReady", () => PointOfInterestTeleporter.onReady());
