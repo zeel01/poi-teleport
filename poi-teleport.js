87c39bf21163a3fab78729f06ec675a3bb5c500f
zeel <zeel02@gmail.com>
2020-09-21 13:58:46 -0400

    Added JSDocs

--- poi-teleport.js
+++ poi-teleport.js
@@ -2,17 +2,42 @@
  * Point of Interest Teleporter
  */
 
+
+/**
+ * @class PointOfInterestTeleporter
+ */
 class PointOfInterestTeleporter {
+	/**
+	 * Handles on the canvasReady Hook.
+	 *
+	 * @static
+	 * @memberof PointOfInterestTeleporter
+	 */
 	static onReady() {
 		canvas.notes.placeables.forEach(n => this.checkNote(n));
 
 		console.log(game.i18n.localize("poitp.name"), "| Ready.");	
 	}
+	/**
+	 * Checks if the supplied note is associated with a scene,
+	 * if so creates a new PointOfInterestTeleporter for that note.
+	 *
+	 * @static
+	 * @param {Note} note - A map note to check 
+	 * @memberof PointOfInterestTeleporter
+	 */
 	static checkNote(note) {
 		const scene = game.scenes.find(s => s.data.journal == note.entry.id);
 		if (scene) new PointOfInterestTeleporter(note, scene); 
 	}
 
+	/**
+	 * Creates an instance of PointOfInterestTeleporter.
+	 * 
+	 * @param {*} note - A map note
+	 * @param {*} scene - A target scene
+	 * @memberof PointOfInterestTeleporter
+	 */
 	constructor(note, scene) {
 		this.note = note;
 		this.scene = scene;
@@ -20,9 +45,19 @@ class PointOfInterestTeleporter {
 		this.activateListeners();
 		
 	}
+	/**
+	 * Activate any event handlers
+	 *
+	 * @memberof PointOfInterestTeleporter
+	 */
 	activateListeners() {
 		this.note.mouseInteractionManager.target.on("rightdown", this._contextMenu.bind(this));
 	}
+	/**
+	 * Handle the right click event
+	 *
+	 * @memberof PointOfInterestTeleporter
+	 */
 	_contextMenu() {
 		this.scene.activate();
 	}
